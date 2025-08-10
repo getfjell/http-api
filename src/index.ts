fae838bc67c0e5a44e744cebfb0b6164995f5031
getfjell <info@getfjell.com>
2025-08-10 07:33:33 -0700

    feat: add support for additional http methods

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,26 @@ export * from "./api/getMethod";
 export * from "./api/http";
 export * from "./api/postMethod";
 export * from "./api/putMethod";
+export * from "./api/optionsMethod";
+export * from "./api/connectMethod";
+export * from "./api/traceMethod";
+export * from "./api/patchMethod";
 export * from "./api/httpFile";
 export * from "./api/postFileMethod";
 export * from "./api/uploadAsyncMethod";
 
 // Explicit exports for TypeScript resolution
-export type { HttpApi, DeleteMethodOptions, GetMethodOptions, PostMethodOptions, PutMethodOptions } from "./api";
+export type {
+  HttpApi,
+  DeleteMethodOptions,
+  GetMethodOptions,
+  PostMethodOptions,
+  PutMethodOptions,
+  OptionsMethodOptions,
+  ConnectMethodOptions,
+  TraceMethodOptions,
+  PatchMethodOptions,
+} from "./api";
 
 // Simple aliases for common use - pre-configured for easy usage
-export { get, post, put, deleteMethod, postFileMethod, uploadAsyncMethod } from "./simple-api";
+export { get, post, put, deleteMethod, postFileMethod, uploadAsyncMethod, options, connect, trace, patch } from "./simple-api";
